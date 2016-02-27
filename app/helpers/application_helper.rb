3c7071ecb41d0c9f8d58eb5957d53dc17c154c0f
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-02-27 12:38:38 +0100

    Add RGeo and test page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def yield_or_default(section, default = '')
     content_for?(section) ? content_for(section) : default
   end
+
+  def support_cell(supported)
+    if supported
+      content_tag :td, 'Yes', class: 'supported'
+    else
+      content_tag :td, 'No', class: 'not-supported'
+    end
+  end
 end
